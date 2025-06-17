02b41fa08fc538d8aeb36572a9f6d2bf8592e473
sounmind <devleesche@gmail.com>
2025-06-16 23:15:51 -0400

    feat: initial unit tests for Dice class functionality

---
+++ tests/DiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use EvanShLee\YachtDice\Dice;
+
+class DiceTest extends TestCase
+{
+  public function testGivenNewDice_WhenRollIsCalled_ThenResultIsBetween1And6() {}
+
+  public function testGivenDiceWithInitialValue_WhenGetValue_ThenReturnsInitialValue() {}
+
+  public function testGivenDice_WhenRolledMultipleTimes_ThenAllResultsAreValidDiceNumbers() {}
+}
